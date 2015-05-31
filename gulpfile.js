087dbf9a5dd6b7b51b37e000eafebd245c724045
saxsir <saxsir.256+github@gmail.com>
2015-05-31 13:11:08 +0900

    [clean] format gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp'),
-    jshint = require('gulp-jshint');
+  jshint = require('gulp-jshint');
 
 gulp.task('default', function() {
-    gulp.watch(['src/*.js', 'gulpfile.js'], ['jshint']);
+  gulp.watch(['src/*.js', 'gulpfile.js'], ['jshint']);
 });
 
 gulp.task('jshint', function() {
-    return gulp.src('./src/*.js')
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('default'))
+  return gulp.src('./src/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
 });

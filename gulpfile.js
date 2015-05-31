7619b9a2927bd7afb5b1c7b6fa482e5d45e5dcc5
saxsir <saxsir.256+github@gmail.com>
2015-05-31 14:13:16 +0900

    Install browserify into local and add browserify task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp'),
-  jshint = require('gulp-jshint');
+  jshint = require('gulp-jshint'),
+  browserify = require('browserify'),
+  source = require('vinyl-source-stream');
 
 gulp.task('default', function() {
-  gulp.watch(['src/*.js', 'gulpfile.js'], ['jshint']);
+  return gulp.watch(['src/*.js', 'gulpfile.js'], ['jshint']);
 });
 
 gulp.task('jshint', function() {
   return gulp.src('./src/*.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
+});
+
+gulp.task('build', function() {
+  return browserify({
+      entries: ['./src/index.js'],
+      extensions: ['.js']
+    })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build'))
 });

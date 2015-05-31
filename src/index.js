59157c3f788b6ed93ae1b92a8a1b7e990c8c9dc0
saxsir <saxsir.256+github@gmail.com>
2015-05-31 14:59:59 +0900

    [wip] Wrote overview of algorithm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
+/**
+ * Webページのデザイン（レイアウト）が、予め決めたどのテンプレートにマッチするか調べるスクリプト
+ * 先行研究におけるClassificationStep
+ *
+ * refs. http://ci.nii.ac.jp/naid/110006951108
+ *
+ */
 var utils = require('./utils');
 
-(function(d) {
+(function() {
   'use strict';
-  console.log(utils.func());
-}(document));
+
+  var Tsize = 320 * 14;    // 横幅は1280を4分割, 高さは一般的な文字サイズ
+
+  var size = utils.getRenderingSize(document.body),
+    preT = [];
+
+  while(size > Tsize) {
+    console.log('size:', size);    // debug
+    var B = [];
+    preT = preT.concat([]);
+    size = size * 0.9;
+  }
+
+  var T = 'T1';
+  return console.log(T);
+}());
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,18 @@
+/**
+ * Webページのデザイン解析でよく使う関数をまとめるファイル
+ */
 (function() {
-  module.exports = {
-    func: function() {
-      console.log('foo');
-    }
+  'use strict';
+
+  function Utils() {}
+
+  // nodeがレンダリングされた時の面積(px)を返す
+  Utils.prototype.getRenderingSize = function(node) {
+    var bounds = node.getBoundingClientRect(),
+      size = bounds.width * bounds.height;
+
+    return size;
   };
+
+  module.exports = new Utils();
 }());

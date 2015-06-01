b88b44c03f12f539bfac66589104bf19e59d3d96
saxsir <saxsir.256+github@gmail.com>
2015-06-01 21:49:56 +0900

    [fix?] 分割サイズが小さいとテンプレートがうまく推定できないので調整

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ var utils = require('./utils');
   // とりあえず保留
   // utils.getPageBounds();
 
-  var Tsize = 320 * 14;    // 横幅は1280を4分割, 高さは一般的な文字サイズ
+  var Tsize = 500 * 500;    // 横幅は1280を4分割, 高さは一般的な文字サイズ
 
   var size = utils.getRenderingSize(document.body),
     preT = [];

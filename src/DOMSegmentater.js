18b3b0ce05e732db7c73edca960e0d138f09a9d7
saxsir <saxsir.256+github@gmail.com>
2015-06-17 18:31:05 +0900

    [wip] エントリーポイントの関数をガワだけ用意

--- src/DOMSegmentater.js
+++ src/DOMSegmentater.js
@@ -5,7 +5,18 @@
   'use strict';
 
   function DOMSegmentater() {
+    var bounds = document.body.getBoundingClientRect();
+    this.pageTop = bounds.top;
+    this.pageLeft = bounds.left;
+    this.pageRight = bounds.right;
+    this.pageBottom = bounds.bottom;
+    this.pageWidth = bounds.width;
+    this.pageHeight = bounds.height;
   }
+
+  DOMSegmentater.prototype.divideDOMToMinimumBlocks = function() {
+    return [];
+  };
 
   module.exports = DOMSegmentater;
 }());
--- src/TemplateClassifier.js
+++ src/TemplateClassifier.js
@@ -4,8 +4,15 @@
 (function() {
   'use strict';
 
+  var Section = require('./Section');
+
   function TemplateClassifier() {
   }
+
+  TemplateClassifier.prototype.matchingTemplate = function(blocks) {
+    //TODO: utils.jsの判定処理をコピーしてくる
+    return -1;
+  };
 
   module.exports = TemplateClassifier;
 }());

2cbef71c96b9df253819579a90df76a22f21368f
saxsir <saxsir.256+github@gmail.com>
2015-06-17 18:23:46 +0900

    [clean] リファクタリング開始
    - 最小ブロックの抽出に大きな修正が必要なことがわかったため
    - ファイルの行数が多くなってきたのでここらで分割

---
+++ src/DOMSegmentater.js
@@ -0,0 +1,11 @@
+/**
+ * ページの分割処理をまとめるクラス
+ */
+(function() {
+  'use strict';
+
+  function DOMSegmentater() {
+  }
+
+  module.exports = DOMSegmentater;
+}());
---
+++ src/TemplateClassifier.js
@@ -0,0 +1,11 @@
+/**
+ * テンプレートを判定（分類）処理をまとめるクラス
+ */
+(function() {
+  'use strict';
+
+  function TemplateClassifier() {
+  }
+
+  module.exports = TemplateClassifier;
+}());
